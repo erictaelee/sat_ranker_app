46f62f5788cd7af54d99cf2c61788627a45e8a50
Kaitlin <klewismurphy@gmail.com>
2021-04-12 18:58:24 -0500

    create app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {
+    this.indexSchools();
+  },
+  methods: {
+    indexSchools: function () {
+      console.log("getting data...");
+      axios.get("https://data.cityofnewyork.us/resource/f9bf-2cp4.json").then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+};
 </script>
