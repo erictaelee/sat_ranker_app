f0a08775c12306153198897395c0cde50151ee65
Kaitlin <klewismurphy@gmail.com>
2021-04-12 19:34:36 -0500

    adds computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-
+    <p> {{ diff }} </p>
   </div>
 </template>
 
@@ -14,7 +14,18 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      schools: [],
+      selected_school: {},
     };
+  },
+  computed: {
+    diff: () => {
+      console.log("diff");
+      return Math.abs(
+        (this.selected_school.sat_critical_reading_avg_score + this.selected_school.sat_writing_avg_score) / 2 -
+          this.selected_school.sat_math_avg_score
+      );
+    },
   },
   created: function () {
     this.indexSchools();
@@ -24,6 +35,8 @@ export default {
       console.log("getting data...");
       axios.get("https://data.cityofnewyork.us/resource/f9bf-2cp4.json").then((response) => {
         console.log(response.data);
+        this.schools = response.data;
+        this.selected_school = this.schools[0];
       });
     },
   },
